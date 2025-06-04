ff678c7a2a4b0d2e76b6dcd07f3dac02fef4ef48
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2025-06-03 22:22:53 -0400

    edits

--- src/components/ForArtists.tsx
+++ src/components/ForArtists.tsx
@@ -6,16 +6,29 @@ const ForArtists: React.FC = () => {
       id="artists"
       className="relative min-h-screen bg-black flex items-center justify-center overflow-hidden"
     >
-      <div className="absolute inset-0">
+      <div className="absolute inset-0 z-0">
         <img
           src="/about-us-bg.png"
           alt="RGB Blur Background"
-          className="w-full h-full"
+          className="w-full h-full object-cover"
         />
+        {/* Black blend over image */}
         <div className="absolute inset-0 bg-black/60 mix-blend-multiply" />
+
+        {/* Top fade overlay */}
+        <div className="absolute top-0 left-0 w-full h-[8rem] bg-gradient-to-b from-black to-transparent pointer-events-none" />
       </div>
 
-      <div className="container mx-auto px-6 md:px-12">hi dwid wdaw daw d</div>
+      <div className="container mx-auto px-6 md:px-12 relative z-10">
+        <h2 className="text-white text-3xl sm:text-4xl md:text-5xl font-bold tracking-tight mb-16 text-center max-w-4xl mx-auto">
+          <p>
+            about
+            <span className="underline ml-4 bg-clip-text text-transparent bg-gradient-to-r from-[#FF0000] to-[#00FF00]">
+              us
+            </span>
+          </p>
+        </h2>
+      </div>
     </section>
   );
 };
--- src/components/Manifesto.tsx
+++ src/components/Manifesto.tsx
@@ -4,40 +4,44 @@ import { Search, Briefcase, Zap } from "lucide-react";
 
 const Vision: React.FC = () => {
   return (
-    <section id="vision" className="py-24 bg-black">
+    <section id="vision" className="pt-8 pb-24 bg-black">
+      <div className="mb-32 text-xl md:border border border-full border-white/60 md:text-2xl p-4 mx-auto w-2/3 text-center text-white/60">
+        "Art commissions still start with a
+        <span className="text-white">&nbsp; cold DM</span> and end with a
+        <span className="text-white">&nbsp; PayPal link</span>. That’s not a
+        creative economy — <u className="text-white">that’s a mess.</u> "
+      </div>
       <div className="container mx-auto px-4 sm:px-6 lg:px-12">
         <h2 className="text-white text-3xl sm:text-4xl md:text-5xl font-bold tracking-tight mb-16 text-center max-w-4xl mx-auto">
           <p>
-            vi$ual's
+            our
             <span className="underline ml-4 bg-clip-text text-transparent bg-gradient-to-r from-[#FF0000] to-[#00FF00]">
               vision
             </span>
           </p>
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 max-w-6xl mx-auto">
+        <div className=" flex flex-col lg:gap-12 max-w-6xl mx-auto">
           {/* Left Column — Vision for Artists */}
           <div className="space-y-6">
             <span className=" text-[#FF0000]">for Artists</span>
-            <div className="md:[70%] flex flex-col">
-              <div className="space-y-4">
-                <FeatureItem
-                  icon={<Users size={32} />}
-                  title="actually get recurring deals"
-                  description="Move beyond one-off gigs. Our platform helps you build ongoing relationships with brands that love your work."
-                />
-                <FeatureItem
-                  icon={<DollarSign size={32} />}
-                  title="decide your rates"
-                  description="No race to the bottom. Set prices that reflect your value and connect with brands willing to pay for quality and authenticity."
-                />
+            <div className=" flex flex-col md:flex-row gap-8 ">
+              <FeatureItem
+                icon={<Users className="text-[#FF0000]" size={32} />}
+                title="actually get recurring deals"
+                description="Move beyond one-off gigs. Our platform helps you build ongoing relationships with brands that love your work."
+              />
+              <FeatureItem
+                icon={<DollarSign className="text-[#FF0000]" size={32} />}
+                title="decide your rates"
+                description="No race to the bottom. Set prices and connect with brands willing to pay for quality and authenticity."
+              />
 
-                <FeatureItem
-                  icon={<Palette size={32} />}
-                  title="keep creating"
-                  description="Create just like you always have. We match you with brands specifically looking for YOUR unique aesthetic."
-                />
-              </div>
+              <FeatureItem
+                icon={<Palette className="text-[#FF0000]" size={32} />}
+                title="keep creating"
+                description="Create just like you always have. We match you with brands specifically looking for YOUR unique aesthetic."
+              />
             </div>
           </div>
           {/* Right Column — Vision for Brands */}
@@ -45,26 +49,24 @@ const Vision: React.FC = () => {
             <p className="bg-clip-text ml-auto text-end text-[#00FF00]">
               for Brands
             </p>
-            <div className="ml-auto md:w-[100%]">
-              <div className="space-y-4">
-                <FeatureItem
-                  icon={<Search className="ml-auto" size={32} />}
-                  title="chatgpt what you're looking for"
-                  description="Search by style, medium, or audience to find artists with our AI that align with your brand aesthetic and audience."
-                />
-
-                <FeatureItem
-                  icon={<Briefcase className="ml-auto" size={32} />}
-                  title="streamlined payments"
-                  description="Clear pricing, straightforward contracts, and secure payments. No more back-and-forth over email or chasing invoices."
-                />
-
-                <FeatureItem
-                  icon={<Zap className="ml-auto" size={32} />}
-                  title="authentic art that converts"
-                  description="Work with artists who understand digital culture and can create content that actually resonates with your target audience."
-                />
-              </div>
+            <div className=" flex flex-col md:flex-row gap-8 ">
+              <FeatureItem
+                icon={<Zap className="ml-auto text-[#00FF00]" size={32} />}
+                title="authentic art that converts"
+                description="Work with artists who understand digital culture & create content that resonates with your target audience."
+              />
+              <FeatureItem
+                icon={<Search className="ml-auto text-[#00FF00]" size={32} />}
+                title="chatgpt what you're looking for"
+                description="Search by style, medium, or audience to find artists with our AI that align with your brand aesthetic and audience."
+              />
+              <FeatureItem
+                icon={
+                  <Briefcase className="ml-auto text-[#00FF00]" size={32} />
+                }
+                title="streamlined payments"
+                description="Clear pricing, straightforward contracts, and secure payments. No more back-and-forth over email or chasing invoices."
+              />
             </div>
           </div>
         </div>
@@ -85,7 +87,7 @@ const FeatureItem: React.FC<FeatureItemProps> = ({
   description,
 }) => {
   return (
-    <div className="">
+    <div className="md:w-1/3 md:h-1/3">
       <div className="absolute inset-0 bg-gradient-to-r from-purple-500/10 to-cyan-500/10 blur group-hover:blur-xl transition-all duration-500 opacity-0 group-hover:opacity-100"></div>
       <div className="relative border border-white/10 p-8 hover:border-white/30 transition-all duration-300">
         <div className="text-cyan-400 mb-4">{icon}</div>
